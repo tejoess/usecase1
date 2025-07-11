ee9efb1903f363ce452de6364c73f297a74f616a
tejoess <tejatsutarofc@gmail.com>
2025-07-11 17:30:39 +0530

    updated app

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import hashlib
 
 # --- Streamlit Page Configuration ---
 st.set_page_config(
-    page_title="URS placeholder values extractor",
+    page_title="URS document",
     page_icon="📄",
     layout="wide"
 )
