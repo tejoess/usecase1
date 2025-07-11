6054e78fcab94c4b7033f7906046cc5288639c12
tejoess <tejatsutarofc@gmail.com>
2025-07-11 17:39:11 +0530

    updated app

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import hashlib
 
 # --- Streamlit Page Configuration ---
 st.set_page_config(
-    page_title="URS document extractor",
+    page_title="URS document extractor v2",
     page_icon="📄",
     layout="wide"
 )
